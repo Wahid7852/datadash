0bd74d53561b2774b77a0152eda2148a67ada475
FrosT2k5 <androygaming@gmail.com>
2024-06-06 21:46:15 +0530

    modularize further
    Signed-off-by: FrosT2k5 <androygaming@gmail.com>

--- Desktop-app/file_receiver.py
+++ Desktop-app/file_receiver.py
@@ -2,8 +2,12 @@ import os
 import platform
 import socket
 import struct
+import threading
 from PyQt6.QtCore import QThread, pyqtSignal
-from PyQt6.QtWidgets import QMessageBox
+from PyQt6.QtWidgets import (
+    QMessageBox, QWidget, QVBoxLayout, QLabel, QProgressBar, QApplication
+)
+from constant import BROADCAST_ADDRESS, BROADCAST_PORT, LISTEN_PORT
 
 RECEIVER_PORT = 12347
 
@@ -50,3 +54,49 @@ class FileReceiver(QThread):
                     received_size += len(data)
                     self.progress_update.emit(received_size * 100 // file_size)
         server_socket.close()
+
+
+class ReceiveApp(QWidget):
+    progress_update = pyqtSignal(int)
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Receive File')
+        self.setGeometry(100, 100, 300, 200)
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel("Waiting for file...", self)
+        layout.addWidget(self.label)
+
+        self.progress_bar = QProgressBar(self)
+        layout.addWidget(self.progress_bar)
+
+        self.setLayout(layout)
+
+        self.file_receiver = FileReceiver()
+        self.file_receiver.progress_update.connect(self.updateProgressBar)
+        self.file_receiver.start()
+
+        threading.Thread(target=self.listenForBroadcast, daemon=True).start()
+
+    def listenForBroadcast(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', BROADCAST_PORT))
+
+            while True:
+                message, address = s.recvfrom(1024)
+                message = message.decode()
+                if message == 'DISCOVER':
+                    response = f'RECEIVER:{socket.gethostname()}'
+                    s.sendto(response.encode(), address)
+
+    def updateProgressBar(self, value):
+        self.progress_bar.setValue(value)
+        if value >= 100:
+            self.label.setText("File received successfully!")
--- Desktop-app/file_sender.py
+++ Desktop-app/file_sender.py
@@ -1,8 +1,12 @@
 import os
 import socket
 import struct
-from PyQt6.QtCore import QThread, pyqtSignal
-from PyQt6.QtWidgets import QMessageBox
+from PyQt6.QtCore import QThread, pyqtSignal, QCoreApplication
+from PyQt6.QtWidgets import (
+    QMessageBox, QWidget, QVBoxLayout, QPushButton, QListWidget, 
+    QProgressBar, QLabel, QFileDialog, QApplication
+)
+from constant import BROADCAST_ADDRESS, BROADCAST_PORT, LISTEN_PORT
 
 RECEIVER_PORT = 12347
 
@@ -38,3 +42,96 @@ class FileSender(QThread):
                 sent_size += len(data)
                 self.progress_update.emit(sent_size * 100 // file_size)
         client_socket.close()
+
+
+class SendApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Send File')
+        self.setGeometry(100, 100, 400, 300)
+
+        layout = QVBoxLayout()
+
+        self.file_button = QPushButton('Select File', self)
+        self.file_button.clicked.connect(self.selectFile)
+        layout.addWidget(self.file_button)
+
+        self.file_path = None
+
+        self.discover_button = QPushButton('Discover Devices', self)
+        self.discover_button.clicked.connect(self.discoverDevices)
+        layout.addWidget(self.discover_button)
+
+        self.device_list = QListWidget(self)
+        layout.addWidget(self.device_list)
+
+        self.send_button = QPushButton('Send File', self)
+        self.send_button.setEnabled(False)
+        self.send_button.clicked.connect(self.sendSelectedFile)
+        layout.addWidget(self.send_button)
+
+        self.progress_bar = QProgressBar(self)
+        layout.addWidget(self.progress_bar)
+
+        self.label = QLabel("", self)
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+    def selectFile(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, 'Open File')
+        if file_path:
+            self.file_path = file_path
+            self.checkReadyToSend()
+
+    def discoverDevices(self):
+        self.device_list.clear()
+        receivers = self.discover_receivers()
+        for receiver in receivers:
+            self.device_list.addItem(f"{receiver['name']} ({receiver['ip']})")
+        self.checkReadyToSend()
+
+    def discover_receivers(self):
+        receivers = []
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', LISTEN_PORT))
+
+            s.sendto(b'DISCOVER', (BROADCAST_ADDRESS, BROADCAST_PORT))
+
+            s.settimeout(2)
+            try:
+                while True:
+                    message, address = s.recvfrom(1024)
+                    message = message.decode()
+                    if message.startswith('RECEIVER:'):
+                        device_name = message.split(':')[1]
+                        receivers.append({'ip': address[0], 'name': device_name})
+            except socket.timeout:
+                pass
+
+        return receivers
+
+    def checkReadyToSend(self):
+        if self.file_path and self.device_list.count() > 0:
+            self.send_button.setEnabled(True)
+
+    def sendSelectedFile(self):
+        selected_item = self.device_list.currentItem()
+        if not selected_item:
+            QMessageBox.critical(None, "Selection Error", "Please select a device to send the file.")
+            return
+        ip_address = selected_item.text().split('(')[-1][:-1]
+        self.send_button.setEnabled(False)
+        self.file_sender = FileSender(ip_address, self.file_path)
+        self.file_sender.progress_update.connect(self.updateProgressBar)
+        self.file_sender.start()
+
+    def updateProgressBar(self, value):
+        self.progress_bar.setValue(value)
+        if value >= 100:
+            self.label.setText("File transfer completed!")
