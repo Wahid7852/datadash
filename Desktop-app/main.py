4f4b49b041401cb34e025f852e8012d6d183f786
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-06-07 08:55:09 +0530

    file rename

--- Desktop-app/main.py
+++ Desktop-app/main.py
@@ -52,6 +52,6 @@ class MainApp(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_app = MainApp()
-    main_app.show()
+    main = MainApp()
+    main.show()
     sys.exit(app.exec())
