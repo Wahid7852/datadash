531b9a79c23e122faec0e879bfd001f4fc9bdd4f
Champion_Samay <142138866+ChampionSamay1644@users.noreply.github.com>
2024-07-09 22:39:51 +0530

    Base Android App made

---
+++ Android-app/app/src/main/java/com/example/crossplatformmediasharing/Receive_files.java
@@ -0,0 +1,12 @@
+package com.example.crossplatformmediasharing;
+
+import android.os.Bundle;
+
+public class Receive_files extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.receive);
+    }
+}
+
