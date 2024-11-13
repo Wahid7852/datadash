b89dce6588477f5e040d7a13278ffacc1055a5b0
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-11-13 16:33:19 +0530

    version number change

--- Android-app/CrossPlatform/app/build.gradle.kts
+++ Android-app/CrossPlatform/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "7.6"
+        versionName = "7.6.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
