97530011f5ddeab8ecfaefbfe515980ef1551125
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-12-17 09:09:35 +0530

    gradle update

--- Android-app/CrossPlatform/app/build.gradle.kts
+++ Android-app/CrossPlatform/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "8.0.8"
+        versionName = "8.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
