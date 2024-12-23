9e16156fd10c199252407bf8eea27b5f60e7f7fc
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-12-24 01:21:20 +0530

    version number change

--- Desktop-app/constant.py
+++ Desktop-app/constant.py
@@ -12,7 +12,7 @@ class ConfigManager(QThread):
     def __init__(self):
         super().__init__()
         self.config_file_name = ".config.json"
-        self.current_version = "4.2.1"
+        self.current_version = "4.2.2"
         self.config_file = self.get_config_file_path()
 
     def get_config_file_path(self):
--- Desktop-app/dd.iss
+++ Desktop-app/dd.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=DataDash
-AppVersion=4.2.1
+AppVersion=4.2.2
 DefaultDirName={commonpf64}\DataDash
 DefaultGroupName=DataDash
 OutputDir=Output
